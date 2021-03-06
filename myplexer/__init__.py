f08e733f06d421642063701d31514e59770f6710
simandhar.sahuji <simandhar.sahuji@screen-magic.com>
2021-03-07 01:53:03 +0530

    test

--- myplexer/__init__.py
+++ myplexer/__init__.py
@@ -1,14 +1,14 @@
-from flask import Flask
-import os
-
-class Myplexer(Flask):
-    def __init__(self):
-        Flask.__init__(self, __name__)
-    # def __init__(self):
-    #     app = Flask("myplexer_app")
-    #     app.config.from_mapping(
-    #         SECRET_KEY='dev')
-
-# app = Flask("myplexer_app")
-# app.config.from_mapping(SECRET_KEY='dev')
-# app = Myplexer()
+# from flask import Flask
+# import os
+#
+# class Myplexer(Flask):
+#     def __init__(self):
+#         Flask.__init__(self, __name__)
+#     # def __init__(self):
+#     #     app = Flask("myplexer_app")
+#     #     app.config.from_mapping(
+#     #         SECRET_KEY='dev')
+#
+# # app = Flask("myplexer_app")
+# # app.config.from_mapping(SECRET_KEY='dev')
+# # app = Myplexer()
---
+++ myplexer/myplexer.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+class Myplexer(Flask):
+    def __init__(self):
+        Flask.__init__(self, __name__)
+    # def __init__(self):
+    #     app = Flask("myplexer_app")
+    #     app.config.from_mapping(
+    #         SECRET_KEY='dev')
+
+# app = Flask("myplexer_app")
+# app.config.from_mapping(SECRET_KEY='dev')
+# app = Myplexer()
+#
+# if __name__ == '__main__':
+#     app.run()
